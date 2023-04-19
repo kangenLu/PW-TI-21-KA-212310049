abc313b9afd51566f737d4fab809ce7b5f800b51
Melani_212310049 <94130388+kangenLu@users.noreply.github.com>
2023-04-20 03:08:25 +0700

    Add files via upload

---
+++ Praktikum04/src/Constdata.js
@@ -0,0 +1,14 @@
+const UserObj = [
+    { id: 1, name: "Zaki", fullname: "Zaki Aljabar", img: require("../assets/feed-image-1.jpg"), gender: "M", is_view: true },
+    { id: 2, name: "Adrian", fullname: "Adrian Adhari", img: require("../assets/feed-image-2.jpg"), gender: "M", is_view: false },
+    { id: 3, name: "Hana", fullname: "Hana Yulia Rahmah", img: require("../assets/feed-image-3.jpg"), gender: "F", is_view: true },
+    { id: 4, name: "Lukman", fullname: "Lukman Nurhakim", img: require("../assets/feed-image-4.jpg"), gender: "M", is_view: true },
+    { id: 5, name: "Cindy", fullname: "Cindy Kevina", img: require("../assets/feed-image-5.jpg"), gender: "F", is_view: false },
+    { id: 6, name: "Erdiana", fullname: "Erdiana Ragil Syawala", img: require("../assets/feed-image-6.jpg"), gender: "F", is_view: true },
+    { id: 7, name: "Michael", fullname: "Michael Fernandez", img: require("../assets/feed-image-7.jpg"), gender: "M", is_view: false },
+    { id: 8, name: "Katarina", fullname: "Katarina Andrea Laurentia", img: require("../assets/feed-image-8.jpg"), gender: "F", is_view: true },
+    { id: 9, name: "Raynaldi", fullname: "Raynaldi Rizky", img: require("../assets/feed-image-9.jpg"), gender: "M", is_view: false },
+    { id: 10, name: "Fauzan", fullname: "Fauzan Rindu", img: require("../assets/feed-image-10.jpg"), gender: "M", is_view: false }
+];
+
+export { UserObj };
